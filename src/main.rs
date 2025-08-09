dae55262491e48cfe1f845ab93ccb7848288e400
Washington Ribeiro <washinuchiha@gmail.com>
2025-08-08 21:33:32 -0300

    init project

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use calculator_washinribeiro::calc1::{add, sub};
+use calculator_washinribeiro::calc2::{multiply, rate};
+
+fn main() {
+    println!("\n---- Testando a Biblioteca Calculatora ----");
+
+    let c = add(3, 8);
+    println!("Soma de 3 + 8: {}", c);
+
+    println!("---- Fim dos testes manuais ----")
+}
