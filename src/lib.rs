7d999b4cf23a416454598c170a0bbb4f4766334a
Washington Ribeiro <washinuchiha@gmail.com>
2025-08-11 07:57:57 -0300

    feat: adding more tests to the project

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 pub mod calc1;
 pub mod calc2;
+pub mod calc3;
 
 #[cfg(test)]
 mod tests {
     use super::calc1::{add, sub};
     use super::calc2::{multiply, rate};
+    use super::calc3::{pot, log};
 
     #[test]
     fn teste_add() {
@@ -15,4 +17,25 @@ mod tests {
     fn teste_sub() {
         assert_eq!(sub(20, 10), 10);
     }
+
+    #[test]
+    fn teste_multiply() {
+        assert_eq!(multiply(2, 3), 6);
+    }
+
+    #[test]
+    fn teste_rate() {
+        assert_eq!(rate(100, 5), 20);
+    }
+
+    #[test]
+    fn teste_pot() {
+        assert_eq!(pot(2, 3), 8);
+    }
+
+    #[test]
+    fn teste_log() {
+        assert_eq!(log(3, 6561), Some(8));
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use calculator_washinribeiro::calc1::{add, sub};
-use calculator_washinribeiro::calc2::{multiply, rate};
+// use calculator_washinribeiro::calc1::{add, sub};
+// use calculator_washinribeiro::calc2::{multiply, rate};
+// use calculator_washinribeiro::calc3::{pot, log};
+use calculator_washinribeiro::calc1::{add};
 
 fn main() {
     println!("\n---- Testando a Biblioteca Calculatora ----");
 
-    let c = add(3, 8);
-    println!("Soma de 3 + 8: {}", c);
+    let c = add(10, 30);
+    println!("Soma de 10 e 30: {:?}", c);
 
     println!("---- Fim dos testes manuais ----")
 }
